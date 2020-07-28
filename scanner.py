bbf803361d5ebbf355fd1f23a103b7840938a127
Sriraam N <sriraamwatto33@gmail.com>
2020-07-28 11:58:57 +0530

    Removed print statement while scanning each port

--- scanner.py
+++ scanner.py
@@ -21,7 +21,6 @@ try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
     result = s.connect_ex((target,port))
-    print('scanning port {}'.format(port))
     if result == 0:
       print('Port {} is open'.format(port))
     s.close()
